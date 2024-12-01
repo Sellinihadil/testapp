c28b2466f6c2c9bea4520a97fb24d9b7d8b9ef5a
Hadil__Sellini <115785628+Sellinihadil@users.noreply.github.com>
2024-12-02 00:23:16 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react"  // Erreur : point-virgule manquant
+
+function App() {
+    return (
+        <div>
+            <h1>Hello World<h1> {/* Erreur : Balise non fermée */}
+            <p> This is a React app </p>
+            <button>Click Me!</button
+        </div>
+    )
+}
+
+export default App  // Erreur : point-virgule manquant
+
