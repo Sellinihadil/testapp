c3a578b55c1cfaaaf0c0ea33b0feae9569a1559c
Hadil__Sellini <115785628+Sellinihadil@users.noreply.github.com>
2024-12-02 11:08:01 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,1 @@
-import React from 'react'
 
-function App () {
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <p>This is a React app</p>
-      <button>Click Me!</button>
-    </div>
-  )
-}
-
-export default App
